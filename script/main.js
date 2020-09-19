90cd366fa62b7966fae4610298ac88b78515734b
VasylHorban <nagatolt427@gmail.com>
2020-09-19 19:50:27 +0300

    'second'

--- script/main.js
+++ script/main.js
@@ -1,9 +1,16 @@
 const getS = selector => document.querySelector(selector);
 
-function generate(){
-    let arr = ['1','2','3','+','-','4','5','6','*','/','7','8','9','(',')','.','0','00', '^', 'sqrt', 'cos', 'sin', 'c', '=','history'];
+function generate() {
+    let arr = ['1', '2', '3', '+', '-', '4', '5', '6', '*', '/', '7', '8', '9', '(', ')', '.', '0', '00', '^', 'sqrt', 'cos', 'sin', 'c', 'history', '='];
     arr.forEach(data => {
         let btn = document.createElement('button');
+        if (data == '=') {
+            btn.classList.add('calculator__button_equal');
+        } else if (data == 'history') {
+            btn.classList.add('calculator__button_history');
+        } else if (data == 'c') {
+            btn.classList.add('calculator__button_clean');
+        }
         btn.setAttribute('data-value', data);
         btn.classList.add('calculator__button')
         btn.textContent = data;
@@ -12,63 +19,70 @@ function generate(){
 }
 generate();
 
-const calculatorModule = (function(){
-    const histori = [];
+
+
+
+const calculatorModule = (function () {
     const outHtml = {};
-    const tempHistory =[];
     let innerData = '';
-    
-    
-    function getHtml(){
-        outHtml.history = getS('.calculator__history_info_output') 
-        outHtml.main = getS('.calculator__main_info_output') 
+    const temp = [];
+    let numbers = '';
+    let expression = [];
+    let expressionIndex = 0;
+
+
+    function getHtml() {
+        outHtml.history = getS('.calculator__history_info_output')
+        outHtml.main = getS('.calculator__main_info_output')
     };
-    function event(){
+
+    function event() {
         getS('.calculator__button_container').addEventListener('click', e => {
-            if(e.target.classList.contains('calculator__button')){
-                culc(e.target.getAttribute('data-value'))
+            if (e.target.classList.contains('calculator__button')) {
+                calc(e.target.getAttribute('data-value'))
             }
         })
     };
-    function save(data){
-        histori.push(data)
-        tempHistory.push(data)
-    }
-    function culc(type){
-        let data;
-        if(!isNaN(parseInt(type))){
-            innerData += type
-            
-            
-        }else{
-            save(innerData)
 
+    function save(data) {
+
+    };
+    
+    function evaluate(str){
+        let result;
+        
+        let numRegExp = /d+\.?d?/
+        console.log(numRegExp.test(str))
+        
+    }
+    
+    function calc(type) {
+        if (type === 'c') {
+            innerData = ''
+
+        } else if (type === '=') {
+            let oldInnerData = innerData;
+            console.log('here')
+            evaluate(innerData)
+           
+        } else {
+            temp.push(numbers)
+            innerData += type
         }
+
         refresh();
     }
-    
-    
-    
-    function refresh(){
+
+    function refresh() {
         outHtml.main.textContent = innerData;
-        console.log(tempHistory.length)
-        console.log(tempHistory)
-        if(tempHistory.length > 1){
-            let out = '';
-            tempHistory.forEach((elem, i )=>{
-               if((tempHistory.length - 1) == i )
-                out += elem
-            })
-            outHtml.history.textContent = out;
-        }
     }
-    
-    function init(){
+
+    function init() {
         event();
         getHtml()
     };
     return {
-        init:init
+        init: init
     };
 })();
 
